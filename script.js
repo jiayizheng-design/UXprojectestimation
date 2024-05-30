157777aa075dfd45a3d10560f3a46008cb1a6450
Jiayi Zheng <46617193+jiayizheng-design@users.noreply.github.com>
2024-05-30 15:17:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -52,9 +52,19 @@ tasks.forEach((task, index) => {
       const optionDiv = document.createElement('div');
       optionDiv.className = 'mb-2';
 
+      const optionCheckbox = document.createElement('input');
+      optionCheckbox.type = 'checkbox';
+      optionCheckbox.className = 'mr-2';
+      optionCheckbox.addEventListener('change', () => toggleSizeOptions(index, option));
+      
       const optionLabel = document.createElement('label');
       optionLabel.innerText = option;
+      optionLabel.prepend(optionCheckbox);
       optionDiv.appendChild(optionLabel);
+
+      const sizeOptionsDiv = document.createElement('div');
+      sizeOptionsDiv.id = `size-options-${index}-${option}`;
+      sizeOptionsDiv.className = 'ml-4 hidden';
 
       Object.keys(sizeLevels).forEach(size => {
         const sizeDiv = document.createElement('div');
@@ -72,18 +82,29 @@ tasks.forEach((task, index) => {
         radioLabel.title = sizeLevels[size].description;
         sizeDiv.appendChild(radioLabel);
 
-        optionDiv.appendChild(sizeDiv);
+        sizeOptionsDiv.appendChild(sizeDiv);
       });
 
+      optionDiv.appendChild(sizeOptionsDiv);
       optionsDiv.appendChild(optionDiv);
     });
   } else {
     const optionDiv = document.createElement('div');
     optionDiv.className = 'mb-2';
 
+    const optionCheckbox = document.createElement('input');
+    optionCheckbox.type = 'checkbox';
+    optionCheckbox.className = 'mr-2';
+    optionCheckbox.addEventListener('change', () => toggleSizeOptions(index));
+    
     const optionLabel = document.createElement('label');
     optionLabel.innerText = task.name;
+    optionLabel.prepend(optionCheckbox);
     optionDiv.appendChild(optionLabel);
+
+    const sizeOptionsDiv = document.createElement('div');
+    sizeOptionsDiv.id = `size-options-${index}`;
+    sizeOptionsDiv.className = 'ml-4 hidden';
 
     Object.keys(sizeLevels).forEach(size => {
       const sizeDiv = document.createElement('div');
@@ -101,9 +122,10 @@ tasks.forEach((task, index) => {
       radioLabel.title = sizeLevels[size].description;
       sizeDiv.appendChild(radioLabel);
 
-      optionDiv.appendChild(sizeDiv);
+      sizeOptionsDiv.appendChild(sizeDiv);
     });
 
+    optionDiv.appendChild(sizeOptionsDiv);
     optionsDiv.appendChild(optionDiv);
   }
 
@@ -116,6 +138,11 @@ function toggleOptions(index) {
   optionsDiv.classList.toggle('hidden');
 }
 
+function toggleSizeOptions(index, option = null) {
+  const sizeOptionsDiv = option ? document.getElementById(`size-options-${index}-${option}`) : document.getElementById(`size-options-${index}`);
+  sizeOptionsDiv.classList.toggle('hidden');
+}
+
 function calculateProjectSize() {
   let totalDays = 0;
   const selectedTasks = [];
@@ -125,24 +152,28 @@ function calculateProjectSize() {
     const optionsDiv = document.getElementById(`options-${index}`);
     if (task.options.length > 0) {
       task.options.forEach(option => {
-        const radios = document.getElementsByName(`size-${index}-${option}`);
-        radios.forEach(radio => {
-          if (radio.checked) {
-            selectedTasks.push(`${task.name} - ${option}`);
-            const sizeValue = radio.value;
-            totalDays += sizeLevels[sizeValue].days;
-          }
-        });
-      });
-    } else {
-      const radios = document.getElementsByName(`size-${index}`);
-      radios.forEach(radio => {
-        if (radio.checked) {
-          selectedTasks.push(task.name);
-          const sizeValue = radio.value;
-          totalDays += sizeLevels[sizeValue].days;
+        const optionCheckbox = optionsDiv.querySelector(`input[type="checkbox"]`);
+        if (optionCheckbox.checked) {
+          const radios = document.getElementsByName(`size-${index}-${option}`);
+          radios.forEach(radio => {
+            if (radio.checked) {
+              selectedTasks.push(`${task.name} - ${option}`);
+              totalDays += sizeLevels[radio.value].days;
+            }
+          });
         }
       });
+    } else {
+      const optionCheckbox = optionsDiv.querySelector(`input[type="checkbox"]`);
+      if (optionCheckbox.checked) {
+        const radios = document.getElementsByName(`size-${index}`);
+        radios.forEach(radio => {
+          if (radio.checked) {
+            selectedTasks.push(task.name);
+            totalDays += sizeLevels[radio.value].days;
+          }
+        });
+      }
     }
   });
 
